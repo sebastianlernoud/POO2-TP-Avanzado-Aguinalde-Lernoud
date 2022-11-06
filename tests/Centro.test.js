49ddd74daaf9d82f6d2d946b38a45d4e788150a4
sebastianlernoud <sebaslernoud@uca.edu.ar>
2022-11-06 18:26:24 -0300

    TDD iteration

--- tests/Centro.test.js
+++ tests/Centro.test.js
@@ -1,4 +1,4 @@
-const Centro = require("../Centro")
+const Centro = require("../src/Centro")
 test("Crear un centro",()=> {
     var centro = new Centro("Local");
     expect(centro.tipo).toBe("Local");
