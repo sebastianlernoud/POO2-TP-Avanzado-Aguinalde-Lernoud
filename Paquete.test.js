877baa576918985a19b6d410cdfa1b18184db6bb
sebastianlernoud <sebaslernoud@uca.edu.ar>
2022-11-06 16:37:25 -0300

    TDD iteration

--- Paquete.test.js
+++ Paquete.test.js
@@ -12,19 +12,19 @@ test("Crear urgencia de paquete",()=>{
 test("Añadir productos al paquete",()=>{
     let paquete = new Paquete("destino 1","Muy rapido");
     paquete.sumarProducto("shampoo",3);
-    expect(paquete.productos).toBe("3 unidades de shampoo,");
+    expect(paquete.productos).toBe("3 unidades de shampoo, ");
 });
 
 
 test("Añadir otro producto al paquete",()=>{
     let paquete = new Paquete("destino 1","Muy rapido");
     paquete.sumarProducto("atun",8);
-    expect(paquete.productos).toBe("8 unidades de atun,");
+    expect(paquete.productos).toBe("8 unidades de atun, ");
 });
 
 test("Añadir varios productos al paquete",()=>{
     let paquete = new Paquete("destino 1","Muy rapido");
     paquete.sumarProducto("atun",8);
     paquete.sumarProducto("Lavarropas",1);
-    expect(paquete.productos).toBe("8 unidades de atun, 1 unidades de Lavarropas,");
+    expect(paquete.productos).toBe("8 unidades de atun, 1 unidades de Lavarropas, ");
 });
