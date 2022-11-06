674485201cb9d7d92da1c482c1c2991264059d74
sebastianlernoud <sebaslernoud@uca.edu.ar>
2022-11-06 16:28:41 -0300

    TDD iteration

--- Paquete.js
+++ Paquete.js
@@ -1,6 +1,11 @@
 function Paquete (destino,urgencia){
     this.destino = destino;
     this.urgencia = urgencia;
+    this.productos;
+    this.sumarProductos(producto,cantidad){
+        this.productos = "3 unidades de shampoo";
+
+    }
 }
 
 module.exports = Paquete;
