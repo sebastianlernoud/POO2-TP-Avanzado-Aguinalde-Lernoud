19dc1b7ffdaa119c8da243334a70182d2a785934
sebastianlernoud <sebaslernoud@uca.edu.ar>
2022-11-06 18:31:48 -0300

    TDD iteration

--- tests/Centro.test.js
+++ tests/Centro.test.js
@@ -1,6 +1,15 @@
 const Centro = require("../src/Centro")
+const Paquete = require("../src/Paquete")
 test("Crear un centro",()=> {
     var centro = new Centro("Local");
     expect(centro.tipo).toBe("Local");
 }
 );
+
+test("Manejar paquetes",()=>{
+    var paquete= new Paquete();
+    var centro = new Centro("Local");
+
+    centro.recibirPaquete(paquete);
+    expect(centro.colaDeEspera()).toEqual([paquete]);
+});
