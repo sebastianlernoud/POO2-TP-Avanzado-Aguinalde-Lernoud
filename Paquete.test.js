8d067060c4b5fde3a74ec32c11bfce82cf4d9091
sebastianlernoud <sebaslernoud@uca.edu.ar>
2022-11-06 16:26:48 -0300

    TDD iteration

--- Paquete.test.js
+++ Paquete.test.js
@@ -8,3 +8,9 @@ test("Crear urgencia de paquete",()=>{
     let paquete = new Paquete("destino 1","Muy rapido");
     expect(paquete.urgencia).toBe("Muy rapido");
 });
+
+test("Añadir productos al paquete",()=>{
+    let paquete = new Paquete("destino 1","Muy rapido");
+    paquete.sumarProducto("shampoo",3);
+    expect(paquete.productos).toBe("3 unidades de shampoo");
+});
