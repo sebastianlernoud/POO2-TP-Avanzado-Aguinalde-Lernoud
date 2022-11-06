a6064e9fa01f26b15d09d865f3ac94ac4c5f86f4
sebastianlernoud <sebaslernoud@uca.edu.ar>
2022-11-06 16:25:07 -0300

    TDD iteration

--- Paquete.js
+++ Paquete.js
@@ -1,5 +1,6 @@
-function Paquete (destino){
+function Paquete (destino,urgencia){
     this.destino = destino;
+    this.urgencia = urgencia;
 }
 
 module.exports = Paquete;
--- Paquete.test.js
+++ Paquete.test.js
@@ -3,3 +3,8 @@ test("Crear paquete",()=>{
     let paquete = new Paquete("destino 1");
     expect(paquete.destino).toBe("destino 1");
 });
+
+test("Crear urgencia de paquete",()=>{
+    let paquete = new Paquete("destino 1","Muy rapido");
+    expect(paquete.urgencia).toBe("Muy rapido");
+});
