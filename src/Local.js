f4942d5e111e51750e10dd4516a23e9d55c77a76
Belen-Eliza <beluella1@gmail.com>
2022-11-07 19:31:48 -0300

    TDD iteration

--- src/Local.js
+++ src/Local.js
@@ -1,5 +1,12 @@
+const Paquete = require("./Paquete");
+
 function Local(){
     this.colaDeSalida=[];
+    this.crearPaquetes=(cantidad)=>{
+        for(let i =0;i<cantidad;i++){
+            this.colaDeSalida.push(new Paquete());
+        }
+    }
 }
 
 module.exports=Local;
