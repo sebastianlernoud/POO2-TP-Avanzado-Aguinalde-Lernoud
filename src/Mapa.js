d4603b2e517706caaa0366ef10888a86419b8705
Belen-Eliza <beluella1@gmail.com>
2022-11-12 17:52:01 -0300

    TDD iteration

--- src/Mapa.js
+++ src/Mapa.js
@@ -10,7 +10,7 @@ function Mapa(filas,columnas){
     this.pasarTurno=()=>{
         //let texto = this.filas[this.cantidadColumnas-1].procesarPaquetes();
         for (let i=this.cantidadColumnas-2;i>0;i--){
-            
+
             this.filas[i].procesarPaquetes();
             if (this.filas[i].colaDeSalida.length!=0){
                 this.filas[i].traspasarPaquete(this.filas[i].colaDeSalida[0],this.filas[i+1]);
@@ -20,6 +20,8 @@ function Mapa(filas,columnas){
         if (this.filas[0].colaDeSalida.length==0){
             this.filas[0].crearPaquetes(4,this.cantidadColumnas);
         } else{
+
+            //paquetes que no pasan a facturacion y no disminuyen su TTL
             this.filas[0].traspasarPaquete(this.filas[0].colaDeSalida[0],this.filas[1]);
         }
     }
