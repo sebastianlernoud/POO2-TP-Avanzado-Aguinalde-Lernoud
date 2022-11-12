bb8f74be6a5eef275ce922b7f1373b140916ec03
Belen-Eliza <beluella1@gmail.com>
2022-11-12 17:12:53 -0300

    TDD iteration

--- src/Mapa.js
+++ src/Mapa.js
@@ -9,14 +9,17 @@ function Mapa(filas,columnas){
 
     this.pasarTurno=()=>{
         let texto = this.filas[this.cantidadColumnas-1].procesarPaquetes();
-        for (let i=this.cantidadColumnas-2;i>=0;i--){
-            if (this.filas[i].colaDeSalida.length!=0){
-                this.filas[1].procesarPaquetes();
+        for (let i=this.cantidadColumnas-2;i>0;i--){
+            if (this.filas[i].colaDeEspera.length!=0){
+                this.filas[i].procesarPaquetes();
                 this.filas[i].traspasarPaquete(this.filas[i].colaDeSalida[0],this.filas[i+1]);
             }
         }
+        
         if (this.filas[0].colaDeSalida.length==0){
             this.filas[0].crearPaquetes(4,this.cantidadColumnas);
+        } else{
+            this.filas[0].traspasarPaquete(this.filas[0].colaDeSalida[0],this.filas[1]);
         }
     }
 }
