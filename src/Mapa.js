cde858c55d67303f3cdfbcf392e7f350a3c59da2
Belen-Eliza <beluella1@gmail.com>
2022-11-12 17:00:25 -0300

    TDD iteration

--- src/Mapa.js
+++ src/Mapa.js
@@ -8,7 +8,7 @@ function Mapa(filas,columnas){
     }
 
     this.pasarTurno=()=>{
-        let texto = this.filas[-1].procesarPaquetes();
+        let texto = this.filas[this.cantidadColumnas-1].procesarPaquetes();
         for (let i=this.cantidadColumnas-2;i>=0;i--){
             if (this.filas[i].colaDeSalida.length!=0){
                 this.filas[i].traspasarPaquete(this.filas[i].colaDeSalida[i],this.filas[i+1]);
