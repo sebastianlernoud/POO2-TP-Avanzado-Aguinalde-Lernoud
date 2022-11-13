eac53857119a897b0d88a3aeaaec51dce8fa4a52
Belen-Eliza <beluella1@gmail.com>
2022-11-13 18:30:56 -0300

    TDD iteration

--- src/Local.js
+++ src/Local.js
@@ -1,14 +1,13 @@
 const Paquete = require("./Paquete");
 
 function Local(centro){
-    this.centro=centro
-    this.colaDeSalida=[];
+    this.centro=centro;
     this.crearPaquetes=(cantidad,columnas)=>{
         if (cantidad>5 || cantidad<=0){
             throw new Error("La cantidad no es valida");
         } 
         for(let i =0;i<cantidad;i++){
-            this.colaDeSalida.push(new Paquete("Destino 1","Rapido",columnas));
+            this.centro.colaDeSalida.push(new Paquete("Destino 1","Rapido",columnas));
         }
     }
     this.procesarPaquetes=()=>{ }
