466e1aa81086bd5c6d69ec22dfab16fa1a96dac3
Belen-Eliza <beluella1@gmail.com>
2022-11-15 20:28:14 -0300

    TDD iteration

--- tests/Centro.test.js
+++ tests/Centro.test.js
@@ -63,18 +63,18 @@ test("Procesar paquetes en un centro de distribucion",()=>{
 });
 
 test("Crear un local",()=>{
-    var local = new Centro(Local());
+    var local = new Centro(Local(),3);
     expect(local.colaDeSalida).toEqual([]);
 });
 
 test("Generar un paquete desde un local",()=>{
-    var local = new Centro(Local());
+    var local = new Centro(Local(),3);
     local.crearPaquetes(1);
     expect(local.colaDeSalida.length).toBe(1);
 });
 
 test("Generar cinco paquetes desde un local",()=>{
-    var local = new Centro(Local());
+    var local = new Centro(Local(),3);
     local.crearPaquetes(5);
     expect(local.colaDeSalida.length).toBe(5);
 });
@@ -87,7 +87,7 @@ test("Error por mas de cinco paquetes desde un local",()=>{
 
 
 test("Pasar un paquete de Local a Centro de Facturacion",()=>{
-    var local = new Centro(Local());
+    var local = new Centro(Local(),3);
     var centroFacturacion = new Centro(new Facturacion(),3);
     local.crearPaquetes(1);
     local.traspasarPaquete(local.colaDeSalida[0],centroFacturacion);
@@ -96,7 +96,7 @@ test("Pasar un paquete de Local a Centro de Facturacion",()=>{
 });
 
 test("Pasar 4 paquetes de Local a Centro de Facturacion con capacidad máxima de 3",()=>{
-    var local = new Centro(Local());
+    var local = new Centro(Local(),3);
     var centroFacturacion = new Centro(new Facturacion(),3);
     local.crearPaquetes(5,4);
     for (let i=0;i<3;i++){
