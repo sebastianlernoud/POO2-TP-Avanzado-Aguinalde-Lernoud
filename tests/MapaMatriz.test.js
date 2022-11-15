d81e5b796b31e9cb4b1ab7aa60186b869eae963b
Belen-Eliza <beluella1@gmail.com>
2022-11-15 20:57:43 -0300

    TDD iteration

--- tests/MapaMatriz.test.js
+++ tests/MapaMatriz.test.js
@@ -6,6 +6,26 @@ const Local = require("../src/Local");
 const Mapa = require("../src/Mapa");
 const Paquete = require("../src/Paquete");
 
-test("",()=>{
+test("Crear una matriz y añadirle centros",()=>{
+    var mapa=new Mapa(2,4);
+
+    let local1=new Centro(new Local(),4);
+    let facturacion1=new Centro(new Facturacion(),5);
+    let calidad1 = new Centro(new Calidad(),3);
+    let distribucion1 = new Centro(new Distribucion(),3);
+
+    let local2=new Centro(new Local(),3);
+    let facturacion2=new Centro(new Facturacion(),5);
+    let calidad2 = new Centro(new Calidad(),3);
+    let distribucion2 = new Centro(new Distribucion(),3);
+
+    mapa.agregarFila([local1,facturacion1,calidad1,distribucion1]);
+    mapa.agregarFila([local2,facturacion2,calidad2,distribucion2])
     
+    expect(mapa.filas[0].length).toBe(4);
+    expect(mapa.filas[1].length).toBe(4);
+    expect(mapa.filas[0][0].colaDeEspera).toBe([]);
 });
+
+// [[l1,l2,l3],//
+//  [f1,f2,f3],[] ]
