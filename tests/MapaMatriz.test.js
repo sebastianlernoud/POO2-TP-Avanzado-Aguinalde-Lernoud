2ac3e7a79efd9c55079ba42fe2dbce816212692c
Belen-Eliza <beluella1@gmail.com>
2022-11-15 21:19:57 -0300

    TDD iteration

--- tests/MapaMatriz.test.js
+++ tests/MapaMatriz.test.js
@@ -43,6 +43,30 @@ test("Crear paquetes y que lleguen a destino teniendo solo una fila",()=>{
     expect(facturacion1.colaDeEspera[1].id).toBe(id2);
 });
 
+test("Crear paquetes y que lleguen a destino teniendo solo una fila",()=>{
+    var mapa=new Mapa(1,4);
+
+    let local1=new Centro(new Local(),4);
+    let facturacion1=new Centro(new Facturacion(),5);
+    let calidad1 = new Centro(new Calidad(),3);
+    let distribucion1 = new Centro(new Distribucion(),3);
+
+
+    mapa.agregarFila([local1,facturacion1,calidad1,distribucion1]);
+    mapa.pasarTurno(2);
+    let id1=local1.colaDeSalida[0].id;
+    let id2=local1.colaDeSalida[1].id;
+    let ttl1=local1.colaDeSalida[0].ttl-4;
+    let ttl2=local1.colaDeSalida[1].ttl-5;
+    let destino = local1.colaDeSalida[0].destino;
+    for (let i=0;i<3;i++){
+        mapa.pasarTurno(0);
+    }
+    
+    expect(mapa.pasarTurno()).toBe("Entregando paquete id "+id1+" al "+destino+", ttl="+ttl1);
+    expect(mapa.pasarTurno()).toBe("Entregando paquete id "+id2+" al "+destino+", ttl="+ttl2);
+});
+
 /* test("Crear paquetes y que lleguen a destino teniendo dos filas",()=>{
     var mapa=new Mapa(1,2);
 
